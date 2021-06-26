f3f7385cebebd4578f4d3a52cc9f4a9e6c756078
leonharetd <LIUSIZE@kingai.cn>
2021-06-26 14:41:45 +0800

    add prototype

---
+++ creation/08_prototype/prototype.go
@@ -0,0 +1,23 @@
+package prototype
+
+type Cloneable interface {
+	Clone() CloneLab
+}
+
+type CloneLab struct {
+    Lab map[string]string
+}
+
+func (l *CloneLab) set(key, value string) {
+	l.Lab[key] = value
+}
+
+func (l *CloneLab) Clone() CloneLab {
+	newMap := make(map[string]string, 0)
+	for k,v := range l.Lab {
+		newMap[k] = v
+	  }
+	return CloneLab{
+		Lab: newMap,
+	}
+}
