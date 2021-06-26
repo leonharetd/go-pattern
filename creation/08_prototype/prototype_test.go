cfc78705c226e558454a9c371a9ab9ff97068ac4
leonharetd <LIUSIZE@kingai.cn>
2021-06-26 14:37:44 +0800

    add prototype

---
+++ creation/08_prototype/prototype_test.go
@@ -0,0 +1,16 @@
+package prototype
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPrototype(t *testing.T) {
+    p := CloneLab{Lab: make(map[string]string)}
+	p.set("dfd", "dsfs")
+	fmt.Println(p.Lab)
+
+	p1 := p.Clone()
+	p1.set("dfd", "clone from p")
+	fmt.Println(p1.Lab)
+}
